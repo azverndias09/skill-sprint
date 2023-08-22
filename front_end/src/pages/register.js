1f0f9af1cc170ed6ea8703bd26dab2273c5ec7de
azverndias09 <azverndias09@gmail.com>
2023-08-22 18:53:30 +0530

    save account register file

--- front_end/src/pages/register.js
+++ front_end/src/pages/register.js
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import emailjs from '@emailjs/browser';
+import { useNavigate } from "react-router-dom";
 const Register = () => {
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [emailError, setEmailError] = useState(false);
     const [name, setName] = useState('');
@@ -13,6 +15,18 @@ const Register = () => {
     const [isRegistered, setIsRegistered] = useState(false);
     const [showOTPInput, setShowOTPInput] = useState(false);
 
+    const [user, setUser] = useState();
+
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("user");
+        if (loggedInUser) {
+          const foundUser = JSON.parse(loggedInUser);
+          setUser(foundUser);
+          navigate("/dashboard");
+        }
+      }, []);
+
     const handleEmailChange = (value) => {
         setEmail(value);
         const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
@@ -21,24 +35,35 @@ const Register = () => {
 
 
     const handleRegister = async () => {
-        const response = await fetch('http://localhost:3001/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name,email, password }),
+        // const response = await fetch('http://localhost:3001/register', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({ name, email, password }),
 
-        });
+        // });
 
-        console.log(response);
-        if (response.ok) {
+        // console.log(response);
+        //   if (response.ok) {
+        if (true) {
             console.log("registration done brother");
+            let userTemp = {
+                name,
+                email
+            };
+            setUser(userTemp);
+            localStorage.setItem('user', JSON.stringify(userTemp));
+            console.log(userTemp);
+            const data = JSON.parse(localStorage.getItem('user'));
+            console.log(data);
+
 
         }
         else {
             console.error("no bro check code");
         }
-        console.log(response);
+        //     console.log(response);
     };
 
     const form = useRef();
@@ -55,18 +80,18 @@ const Register = () => {
             message: newGeneratedOtp,
         };
 
-        emailjs
-            .send('service_5dqwn3h', 'template_309hj8f', templateParams, 'fg0vs7jHRI7Fm0CsK')
-            .then(
-                (result) => {
+        // emailjs
+        //     .send('service_5dqwn3h', 'template_309hj8f', templateParams, 'fg0vs7jHRI7Fm0CsK')
+        //     .then(
+        //         (result) => {
 
-                    setShowOTPInput(true);
-                },
-                (error) => {
-                    //error stuff here
-                }
-            );
-       /// setShowOTPInput(true);
+        //             setShowOTPInput(true);
+        //         },
+        //         (error) => {
+        //             //error stuff here
+        //         }
+        //     );
+        setShowOTPInput(true);
 
     };
 
@@ -74,6 +99,7 @@ const Register = () => {
         if (testOtp == generatedOtp) {
             console.log("OTP is correct!");
             setIsRegistered(true);
+            handleRegister();
         } else {
             console.log("OTP is incorrect!");
             console.log(generatedOtp);
@@ -82,7 +108,7 @@ const Register = () => {
         }
     };
 
-    return (
+    return user ? <div>{user.name} is loggged in</div> : (
         <>
             <h1>Register</h1>
             <form ref={form} onSubmit={sendOTP}>
@@ -112,7 +138,7 @@ const Register = () => {
                 <div>
                     <TextField
                         label="Password"
-                        type="password"         
+                        type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         variant="outlined"
