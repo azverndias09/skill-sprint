924e104ab4470fb81660d3c03e5d2ad4d6b945b9
azverndias09 <azverndias09@gmail.com>
2023-10-13 13:28:39 +0530

    made project stable

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 //import App from './App';
@@ -37,12 +37,11 @@ export default function App() {
     </BrowserRouter>
   );
 }
-const root = createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
