6c89244ca4d153c6f0f9d6ad383821a011178b45
jasonmiranda99 <jasonmiranda999@gmail.com>
2023-11-13 12:52:29 +0530

    minor changes

--- back_end/routes/register.js
+++ back_end/routes/register.js
@@ -26,7 +26,6 @@ db.getConnection((err, connection) => {
 
 const bcrypt = require("bcrypt")
 const saltRounds=10
-router.use(express.json())
 
 router.post('/',async (req,res)=>{
     const user = req.body.name;
@@ -68,3 +67,4 @@ router.post('/',async (req,res)=>{
 });//end of router.post()
 
 
+modules.exports=router
