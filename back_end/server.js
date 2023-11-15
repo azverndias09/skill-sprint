1069117ee6cb2d818c42ffd21ac16a44c91f804a
jasonmiranda99 <jasonmiranda999@gmail.com>
2023-11-15 09:03:30 +0530

    added businesshome backend

--- back_end/server.js
+++ back_end/server.js
@@ -13,14 +13,10 @@ const clientprofileRoute=require("./routes/clientprofile");
 const businessprofileRoute = require("./routes/businessprofile");
 const createadRoute = require("./routes/createad");
 const clienthomeRoute = require("./routes/clienthome");
+const businesshomeRoute = require("./routes/businesshome");
 const displaybusinessprofileRoute = require("./routes/displaybusinessprofile");
 const displayclientprofileRoute = require("./routes/displayclientprofile");
 const servicepageRoute = require("./routes/servicepage");
-
-
-
-
-
 
 
 const app = express();
@@ -31,9 +27,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
 app.use("/user", userRoute);
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -41,6 +34,7 @@ app.use("/resetPass", resetpassRoute);
 app.use("/clientprofile",clientprofileRoute);
 app.use("/businessprofile", businessprofileRoute);
 app.use("/clienthome", clienthomeRoute);
+app.use("/businesshome", businesshomeRoute);
 app.use("/createad", createadRoute);
 app.use("/api/payment/", paymentRoutes);
 app.use("/location", locationRoute);
@@ -49,7 +43,6 @@ app.use("/displayclientprofile", displayclientprofileRoute);
 app.use("/servicepage", servicepageRoute);
 
 
-app.use(express.static('routes'));
 
 
 //setupChat(server);
